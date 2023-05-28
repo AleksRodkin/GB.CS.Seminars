af600b8051f599b5a6301bdc7e15529d06fe7c73
AleksRodkin <aleksrodkin95@gmail.com>
2023-05-28 17:11:02 +0300

    Completed(?) Task 60

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -33,54 +33,54 @@ void PrintArray(int[] array)
 
 // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
-void PrintArrayNoSquareBrackets(int[] array)
-{
-  for (int i = 0; i < array.Length - 1; i++)
-  {
-    Console.Write(array[i] + " ");
-  }
-  Console.Write(array[array.Length - 1]);
-  Console.WriteLine();
-}
+// void PrintArrayNoSquareBrackets(int[] array)
+// {
+//   for (int i = 0; i < array.Length - 1; i++)
+//   {
+//     Console.Write(array[i] + " ");
+//   }
+//   Console.Write(array[array.Length - 1]);
+//   Console.WriteLine();
+// }
 
-int[] StoogeSort(int[] array, int startIndex, int endIndex)
-{
-  if (array[startIndex] < array[endIndex])
-  {
-    var temp = array[startIndex];
-    array[startIndex] = array[endIndex];
-    array[endIndex] = temp;
-  }
+// int[] StoogeSort(int[] array, int startIndex, int endIndex)
+// {
+//   if (array[startIndex] < array[endIndex])
+//   {
+//     var temp = array[startIndex];
+//     array[startIndex] = array[endIndex];
+//     array[endIndex] = temp;
+//   }
 
-  if (endIndex - startIndex > 1)
-  {
-    var length = (endIndex - startIndex + 1) / 3;
-    StoogeSort(array, startIndex, endIndex - length);
-    StoogeSort(array, startIndex + length, endIndex);
-    StoogeSort(array, startIndex, endIndex - length);
-  }
+//   if (endIndex - startIndex > 1)
+//   {
+//     var length = (endIndex - startIndex + 1) / 3;
+//     StoogeSort(array, startIndex, endIndex - length);
+//     StoogeSort(array, startIndex + length, endIndex);
+//     StoogeSort(array, startIndex, endIndex - length);
+//   }
 
-  return array;
-}
+//   return array;
+// }
 
-void SortAndPrint2dArrayRows(int[,] array)
-{
-  int[] rowArray = new int[array.GetLength(1)];
+// void SortAndPrint2dArrayRows(int[,] array)
+// {
+//   int[] rowArray = new int[array.GetLength(1)];
 
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      rowArray[j] = array[i, j];
-    }
-    StoogeSort(rowArray, 0, array.GetLength(1) - 1);
-    PrintArrayNoSquareBrackets(rowArray);
-  }
-}
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       rowArray[j] = array[i, j];
+//     }
+//     StoogeSort(rowArray, 0, array.GetLength(1) - 1);
+//     PrintArrayNoSquareBrackets(rowArray);
+//   }
+// }
 
-int[,] myArray = CreateRandom2dArray(3, 5, 10, 99);
-Show2dArray(myArray);
-SortAndPrint2dArrayRows(myArray);
+// int[,] myArray = CreateRandom2dArray(3, 5, 10, 99);
+// Show2dArray(myArray);
+// SortAndPrint2dArrayRows(myArray);
 
 
 
@@ -89,39 +89,39 @@ SortAndPrint2dArrayRows(myArray);
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
-int[] FindSumInRows(int[,] array)
-{
-	  int[] sumArray = new int[array.GetLength(0)];
+// int[] FindSumInRows(int[,] array)
+// {
+//   int[] sumArray = new int[array.GetLength(0)];
 
-  for (int i = 0; i < array.GetLength(0); i++)
-    for (int j = 0; j < array.GetLength(1); j++)
-		{
-			sumArray[i] += array[i,j];	
-		}
+//   for (int i = 0; i < array.GetLength(0); i++)
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       sumArray[i] += array[i, j];
+//     }
 
-	return sumArray;
-}
+//   return sumArray;
+// }
 
-int FindMinInArray(int[] array)
-{
-	int min = array[0];
-	int minIndex = 0;
-	for(int i = 1; i < array.Length; i++)
-	{
-		if (array[i] < min)
-		{
-			min = array[i];
-			minIndex = i;
-		}
-	}
-	return minIndex;
-}
+// int FindMinInArray(int[] array)
+// {
+//   int min = array[0];
+//   int minIndex = 0;
+//   for (int i = 1; i < array.Length; i++)
+//   {
+//     if (array[i] < min)
+//     {
+//       min = array[i];
+//       minIndex = i;
+//     }
+//   }
+//   return minIndex;
+// }
 
-int[,] myNewArray = CreateRandom2dArray(3, 5, 0, 3);
-Show2dArray(myNewArray);
-int[] sumInRowsArray = FindSumInRows(myNewArray);
-PrintArray(sumInRowsArray);
-Console.WriteLine(FindMinInArray(sumInRowsArray));
+// int[,] myNewArray = CreateRandom2dArray(3, 5, 0, 3);
+// Show2dArray(myNewArray);
+// int[] sumInRowsArray = FindSumInRows(myNewArray);
+// PrintArray(sumInRowsArray);
+// Console.WriteLine(FindMinInArray(sumInRowsArray));
 
 
 
@@ -138,38 +138,57 @@ Console.WriteLine(FindMinInArray(sumInRowsArray));
 // 18 20
 // 15 18
 
-int[,] multiplyTwoMatrixes(int[,] A, int[,] B)
-{
-  int M = A.GetLength(0);
-  int N = B.GetLength(1);
-  int K = A.GetLength(1);
-  int[,] C = new int[M, N];
-  for (int i = 0; i < M; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
-      for (int k = 0; k < K; k++)
-        C[i, j] += A[i, k] * B[k, j];
-    }
-  }
-  return C;
-}
+// int[,] multiplyTwoMatrixes(int[,] A, int[,] B)
+// {
+//   int M = A.GetLength(0);
+//   int N = B.GetLength(1);
+//   int K = A.GetLength(1);
+//   int[,] C = new int[M, N];
+//   for (int i = 0; i < M; i++)
+//   {
+//     for (int j = 0; j < N; j++)
+//     {
+//       for (int k = 0; k < K; k++)
+//         C[i, j] += A[i, k] * B[k, j];
+//     }
+//   }
+//   return C;
+// }
 
-int[,] firstMatrix = CreateRandom2dArray(3, 3, 0, 9);
-Show2dArray(firstMatrix);
-int[,] secondMatrix = CreateRandom2dArray(3, 3, 0, 9);
-Show2dArray(secondMatrix);
-int[,] resultMatrix = multiplyTwoMatrixes(firstMatrix, secondMatrix);
-Show2dArray(resultMatrix);
+// int[,] firstMatrix = CreateRandom2dArray(3, 3, 0, 9);
+// Show2dArray(firstMatrix);
+// int[,] secondMatrix = CreateRandom2dArray(3, 3, 0, 9);
+// Show2dArray(secondMatrix);
+// int[,] resultMatrix = multiplyTwoMatrixes(firstMatrix, secondMatrix);
+// Show2dArray(resultMatrix);
 
 
 
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
+
+int[,,] new3dArray = {{ {14, 45, 32}, { 54, 15, 42}, { 25, 63, 43 }},
+                    	{ {19, 84, 66}, { 95, 46, 21}, { 31, 12, 88 }},
+                    	{ {17, 27, 33}, { 64, 62, 44}, { 76, 47, 29 }}};
+
+void Show3dArray(int[,,] array)
+{
+  for (int k = 0; k < array.GetLength(2); k++)
+  {
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+        Console.Write(array[i, j, k] + $"({i},{j},{k}) ");
+      }
+			Console.WriteLine();
+    }
+		Console.WriteLine();
+  }
+}
+
+Show3dArray(new3dArray);
+
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
